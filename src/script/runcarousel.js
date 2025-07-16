a6e66fad41da538974187295ff669088604e8713
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-07-16 08:04:52 +0530

    WIP
    WIP
    
    WIP
    
    WIP
    
    Done, almost...
    
    Done

--- src/script/runcarousel.js
+++ src/script/runcarousel.js
@@ -1,23 +1,110 @@
 function runCarousel() {
-  const carousel = document.querySelector(".carousel");
+  // const carousel = document.querySelector(".carousel");
+  const slidesContainer = document.querySelector(".slides-container");
+  const carouselNav = document.querySelector(".carousel-nav");
   const carouselButtonPrev = document.querySelector(".carousel-button--prev");
   const carouselButtonNext = document.querySelector(".carousel-button--next");
-  const slidesContainer = document.querySelector(".slides-container");
-  const slideList = document.querySelectorAll(".carousel-slide");
-  const carouselNav = document.querySelector(".carousel-nav");
-  const imageWidth = carousel.getBoundingClientRect().width;
+  let allSlidesTemp = document.querySelectorAll(".slide");
+  let firstSlide = allSlidesTemp[0].cloneNode(true);
+  let lastSlide = allSlidesTemp[allSlidesTemp.length - 1].cloneNode(true);
   let navBtn;
-  for (let i = 0; i < slideList.length; i++) {
-    slideList[i].style.left = imageWidth * i + "px";
+  slidesContainer.prepend(lastSlide);
+  slidesContainer.append(firstSlide);
+  allSlidesTemp = null;
+  firstSlide = null;
+  lastSlide = null;
+  const allSlides = document.querySelectorAll(".slide");
+  const imageWidth = allSlides[0].getBoundingClientRect().width;
+  let activeSlide = 1;
+  let targetSlide = 1;
+  let isHoveringSlide = false;
+
+  for (let i = 0; i < allSlides.length; i++) {
+    allSlides[i].style.left = imageWidth * i + "px";
     navBtn = document.createElement("button");
+    navBtn.innerText = i;
     navBtn.classList.add("carousel-nav-button");
+    // if (i === 0 || i === allSlides.length - 1) {
+    //   navBtn.display.style = "none";
+    // }
     carouselNav.appendChild(navBtn);
     navBtn = null;
   }
-  const carouselNavButtons = document.querySelectorAll(".carousel-nav-button");
-  carouselNavButtons[0].classList.add("active-slide");
-  slideList[0].classList.add("active-slide");
-}
+  const navButtons = document.querySelectorAll(".carousel-nav-button");
+  navButtons[0].style.display = "none";
+  navButtons[allSlides.length - 1].style.display = "none";
+  updateCarousel();
+  carouselButtonNext.addEventListener("click", nextSlide);
+  carouselButtonPrev.addEventListener("click", previousSlide);
+  carouselNav.addEventListener("click", (e) => {
+    let clickedButton = e.target.closest("button");
+    for (let i = 1; i < navButtons.length - 1; i++) {
+      if (navButtons[i] === clickedButton) {
+        targetSlide = i;
+        break;
+      }
+    }
+    updateCarousel();
+  });
+  //Auto loop slide
+  slidesContainer.addEventListener("mouseleave", () => {
+    isHoveringSlide=false;
+  });
 
+  slidesContainer.addEventListener("mouseenter", () => {
+    isHoveringSlide = true;
+  });
+
+  function autoLoopSlides() {
+    if (isHoveringSlide) {
+      return;
+    }
+    nextSlide();
+  }
+
+  setInterval(autoLoopSlides, 5000);
+
+  //end of Auto loop slide
+
+  function nextSlide() {
+    if (activeSlide < allSlides.length - 1) {
+      targetSlide = activeSlide + 1;
+      updateCarousel();
+    }
+  }
+
+  function previousSlide() {
+    if (activeSlide > 0) {
+      targetSlide = activeSlide - 1;
+      updateCarousel();
+    }
+  }
+
+  function updateCarousel() {
+    slidesContainer.style.transition = "transform 0.3s ease-in-out";
+    slidesContainer.style.transform =
+      "translateX(-" + targetSlide * imageWidth + "px)";
+    navButtons[activeSlide].classList.remove("active-slide");
+    navButtons[targetSlide].classList.add("active-slide");
+    activeSlide = targetSlide;
+    targetSlide = -1;
+    if (activeSlide === 0) {
+      navButtons[activeSlide].classList.remove("active-slide");
+      activeSlide = allSlides.length - 2;
+      navButtons[activeSlide].classList.add("active-slide");
+      slidesContainer.style.transition = "none";
+      slidesContainer.style.transform =
+        "translateX(-" + activeSlide * imageWidth + "px)";
+      //slidesContainer.style.transition = "transform 0.5s ease-in-out";
+    } else if (activeSlide === allSlides.length - 1) {
+      navButtons[activeSlide].classList.remove("active-slide");
+      activeSlide = 1;
+      navButtons[activeSlide].classList.add("active-slide");
+      slidesContainer.style.transition = "none";
+      slidesContainer.style.transform =
+        "translateX(-" + activeSlide * imageWidth + "px)";
+      //slidesContainer.style.transition = "transform 0.5s ease-in-out";
+    }
+  }
+}
 runCarousel();
-// getBoundingClientRect().width
